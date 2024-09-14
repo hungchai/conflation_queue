9879a4c61ffe785ee6bf6c78ceac12a398d4d993
tomma <hungchai08@yahoo.com.hk>
2024-09-14 09:17:37 +0800

    low gc version

--- src/test/java/tomma/hft/conflatingqueue/ConflatingQueueImplTest.java
+++ src/test/java/tomma/hft/conflatingqueue/ConflatingQueueImplTest.java
@@ -13,8 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConflatingQueueImplTest {
     private ConflatingQueueImpl<String, Long> conflationQueue;
-    private static final long TOTAL = 10_000_000;
-    final int keyCount = 2 * 5000;
+    private static final long TOTAL = 100_000_000;
+    final int keyCount = 2 * 5000 +1;
     final String END_KEY = "KEY_END";
 
     @BeforeEach
@@ -26,7 +26,7 @@ class ConflatingQueueImplTest {
     void offer() {
         conflationQueue = new ConflatingQueueImpl<>(keyCount);
 
-        final List<String> keys = new ArrayList<>(keyCount + 1);
+        final List<String> keys = new ArrayList<>(keyCount);
         for (int i = 0; i < keyCount; i++) keys.add("KEY_" + i);
         keys.add(END_KEY);
         final Random rnd = new Random();
